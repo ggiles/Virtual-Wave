3223945b6fa194558f6b914b9b91e74f5796d7a8
Steven Edwon <edwon@me.com>
2014-10-18 00:30:37 +0900

    wave experiments

---
+++ Assets/00_Everything/Scripts/WaveLineGenerator.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vectrosity;
+
+public class WaveLineGenerator : MonoBehaviour {
+
+	public int totalLines = 4;
+	VectorLine[] waveLines;
+	Vector3[] waveLinesPoints;
+	WaveParticleSpring wps;
+
+	void Start () {
+
+		waveLines = new VectorLine[totalLines];
+		waveLinesPoints = new Vector3[totalLines*2];
+		wps = gameObject.GetComponent<WaveParticleSpring>();
+
+		// render all the lines intitially
+		for (int i = 0; i < waveLines.Length ; i++)
+		{
+			if (wps.targetObjects[i] != null)
+			{
+				waveLinesPoints[i*2] = transform.position;
+				Vector3 targetPosition = wps.targetObjects[i].position;
+				waveLinesPoints[i*2+1] = targetPosition;
+				waveLines[i] = new VectorLine("MyLine", waveLinesPoints, Color.red, null, 2.0f);
+			}
+		}
+	}
+	
+	void Update () {
+
+		// render all the lines each update
+		for (int i = 0; i < waveLines.Length ; i++)
+		{
+			if (wps.targetObjects[i] != null)
+			{
+				waveLinesPoints[i*2] = transform.position;
+				Vector3 targetPosition = wps.targetObjects[i].position;
+				waveLinesPoints[i*2+1] = targetPosition;
+				waveLines[i].Draw();
+			}
+		}
+
+	}
+}
